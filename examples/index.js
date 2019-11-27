fde4adf905ba00a7c643dd63aa29fc5ef58aa9ba
Jake Cooper <jake.elijah.cooper@gmail.com>
2019-11-27 00:31:09 -0800

    Default and alternate in file

--- examples/index.js
+++ examples/index.js
@@ -12,13 +12,11 @@ const runner = async () => {
   //while (true) {
   // db.Put(uuid(), { [uuid()]: uuid() });
   // db.delete("e22975eb-1506-4881-b1ea-efaad7d44b8d");
-  let db = easyDB.connect({
-    uuid: "9785cd23-6a42-4659-8518-713a99c77abb",
-    token: "750f1876-f8e4-45fe-9f97-87805b893980"
-  });
+  //let db = easyDB.connect("alternate");
+  // console.log("IT IS", db.connect);
   // db.put("hello", "world");
   //db.put("hello", "Jake");
-  const data = await db.list((err, data) => console.log(data));
+  const data = await easyDB.list((err, data) => console.log(data));
   console.log(data);
   //}
 };
--- src/index.js
+++ src/index.js
@@ -46,7 +46,8 @@ let DEFAULT_DB;
 
 if (isNode && fs.existsSync(CONFIG_FILE)) {
   let data = JSON.parse(fs.readFileSync(CONFIG_FILE));
-  DEFAULT_DB = data;
+  let { uuid, token } = data.databases.default;
+  DEFAULT_DB = { uuid, token, url: data.url };
 }
 
 const instanceActions = {
@@ -109,11 +110,36 @@ const constructActions = database => {
   }, {});
 };
 
-const connect = ({ uuid, token, url = BASE_URL }) => {
+const connect = params => {
+  let uuid, token;
+  let configData = JSON.parse(fs.readFileSync(CONFIG_FILE));
+
+  if (typeof params === "string" || params == null) {
+    // Initialize from config
+    if (!params) {
+      // Nothing provided, connect to default
+      params = "default";
+    }
+    if (!configData.databases[params]) {
+      let validChoices = Object.keys(configData.databases)
+        .map(s => ` - ${s}`)
+        .join("\n");
+      throw `Could not find database ${params} in easydb.config.json\n Valid databases are: \n${validChoices}`;
+    }
+    let database = configData.databases[params];
+    uuid = database.uuid;
+    token = database.token;
+  } else if (typeof params === "object") {
+    // Initialize from object
+    uuid = params.uuid;
+    token = params.token;
+  } else {
+    throw "Invalid parameters passed to connect(Object | String)";
+  }
   if (!uuid || !token) {
     throw "uuid and token must be provided";
   }
-  let currentDB = { uuid, token, url };
+  let currentDB = { uuid, token, url: configData.url };
   return constructActions(currentDB);
 };
 
